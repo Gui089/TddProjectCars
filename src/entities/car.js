d11f9f73b895277cd9eeaecf253a1386bd73a121
Gui089 <guilherme@gui>
2024-10-24 10:41:15 -0300

    feat: database ready

--- src/entities/car.js
+++ src/entities/car.js
@@ -1,7 +1,13 @@
-const base = require('./base/base');
+const Base = require('./base/base');
 
 class Car extends Base {
+    constructor({id,name,releaseYear, available, gasAvailable}) {
+        super({id,name});
 
+        this.releaseYear = releaseYear
+        this.available = available
+        this.gasAvailable = gasAvailable
+    }
 }
 
 module.exports = Car;
